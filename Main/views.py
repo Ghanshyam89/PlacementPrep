042095bd374f77ce97e977b3a1767a234dba5c8d
aimanasif27 <aimanasif2799@gmail.com>
2022-08-26 21:52:57 +0530

    Initiated Main Dashboard

--- Main/views.py
+++ Main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def loginRegister(request):
-    return render(request, 'loginRegister.html')
+def main(request):
+    return render(request, 'main.html')
 
+    
