6ef6a1ba73061486ee80cc4456a03295c93962b0
Ghanshyam89 <ghanshyampraja8959@gmail.com>
2022-09-10 01:18:20 +0530

    Modified Register/views.py

--- Dashboard/models.py
+++ Dashboard/models.py
@@ -21,8 +21,11 @@ class Questions(models.Model):
 
 
 class Student(models.Model):
-    user = models.ForeignKey(User, blank = True, null = True, on_delete = models.CASCADE)
+    first_name = models.CharField('First Name', max_length=15, blank = True)
+    last_name = models.CharField('Last Name', max_length=15, blank = True)
+    username = models.CharField('Username', max_length=30, blank = True)
+    email = models.EmailField('User Email', blank = True)
     que_ids = models.ManyToManyField(Questions, blank = True) 
     
     def __str__(self):
-        return self.user.first_name+' '+self.user.last_name
+        return self.first_name+' '+self.last_name
--- Register/views.py
+++ Register/views.py
@@ -3,9 +3,11 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.models import User
 from .forms import RegistrationForm
+from Dashboard.models import Student
 # Create your views here.
 
 def register(request):
+
     if request.method == "POST":
         form = RegistrationForm(request.POST)
         if form.is_valid():
@@ -14,8 +16,17 @@ def register(request):
             user.last_name = form.cleaned_data['last_name']
             user.username = form.cleaned_data['username']
             user.email = form.cleaned_data['email']
-            user.password = form.cleaned_data['password']
+            # user.password = form.cleaned_data['password']
+            user.set_password(form.cleaned_data['password'])
             user.save()
+
+            # Saving data in Student Model
+            student = Student(first_name = form.cleaned_data['first_name'],
+                            last_name = form.cleaned_data['last_name'],
+                            username = form.cleaned_data['username'],
+                            email = form.cleaned_data['email'])
+            student.save()
+
             return HttpResponseRedirect('/login', "user created")
     else:
         form = RegistrationForm()
