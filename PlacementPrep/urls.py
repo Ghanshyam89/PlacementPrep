1c3ce8d43bfa35f1483a62e09f07926f265fb14a
Ghanshyam89 <ghanshyampraja8959@gmail.com>
2022-09-11 10:43:59 +0530

    Fixed url

--- PlacementPrep/urls.py
+++ PlacementPrep/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('django.contrib.auth.urls')),
+    path('', include('Register.urls')),
     path('register/', include('Register.urls')),
     path('main/', include('Dashboard.urls')),
     path('quiz/', include('Quiz.urls')),
