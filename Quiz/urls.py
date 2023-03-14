58cf828822753536564cc4e1970cca95247049f0
Esha Verma <eshaverma8302@gmail.com>
2023-03-14 23:22:11 +0530

    added quiz

--- Quiz/urls.py
+++ Quiz/urls.py
@@ -1,7 +1,10 @@
 from django.urls import URLPattern, path
+from django.views.decorators.csrf import csrf_exempt
+
 
 from . import views
 
 urlpatterns = [
-    path("", views.quiz, name='quiz'),
+    path("", csrf_exempt(views.quiz), name='quiz'),
+    path("results/", views.quiz_results, name='quiz_results'),
 ]
