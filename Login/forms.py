c3d13ad62a8a1a2eb211edf9fbb6e4365cd8717e
Esha Verma <eshaverma8302@gmail.com>
2023-04-09 20:33:42 +0530

    migration changes

--- Login/forms.py
+++ Login/forms.py
@@ -1,6 +1,5 @@
 # from socket import fromshare
 from django import forms
-    
 class LoginForm(forms.Form):
     username = forms.CharField(label='Username',
                                 max_length=100,
--- PlacementPrep/settings.py
+++ PlacementPrep/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'Login',
     'Videos',
     'Contests',
+    'Career',
 ]
 
 MIDDLEWARE = [
