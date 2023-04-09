1ced140c3b9405b4db42bdb050a13c23384ca338
Esha Verma <eshaverma8302@gmail.com>
2023-04-09 20:33:01 +0530

    added validation to register form

--- Register/forms.py
+++ Register/forms.py
@@ -1,5 +1,13 @@
 # from socket import fromshare
 from django import forms
+from django.core.validators import RegexValidator
+
+fname_validator = RegexValidator(r'^[a-zA-Z ]+$', "First Name can only contain Alphabets !")
+
+lname_validator = RegexValidator(r'^[a-zA-Z ]+$', "Last Name can only contain Alphabets !")
+# Minimum eight characters, at least one letter, one number and one special character:
+
+psw_validator = RegexValidator(r'^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$', "Minimum eight characters, at least one letter, one number and one special character required in password !")
 
 class RegistrationForm(forms.Form):
     first_name = forms.CharField(label='First name', 
@@ -8,14 +16,16 @@ class RegistrationForm(forms.Form):
                                     'class':'form-control',
                                     'type': 'text',
                                     'id':'registerFirstName'
-                                }))
+                                }),
+                                validators = [fname_validator])
     last_name = forms.CharField(label='Last name',
                                 max_length=100,
                                 widget=forms.TextInput(attrs={
                                     'class':'form-control',
                                     'type': 'text',
                                     'id':'registerLastName'
-                                }))
+                                }),
+                                validators = [lname_validator])
     username = forms.CharField(label='Username',
                                 max_length=100,
                                 widget=forms.TextInput(attrs={
@@ -36,7 +46,8 @@ class RegistrationForm(forms.Form):
                                     'class':'form-control',
                                     'type': 'password',
                                     'id':'registerPassword'
-                                }))
+                                }),
+                                validators = [psw_validator])
     repeat_password =  forms.CharField(label='Repeat Password',
                                 max_length=100,
                                 widget=forms.TextInput(attrs={
